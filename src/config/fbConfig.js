41a3159fee1408622a08602e5486e2586610973a
Hetavi <31500256+Hetavi@users.noreply.github.com>
2020-05-28 01:30:16 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,9 @@ import 'firebase/auth';
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyBF7m7VhZSwUCH_U5NbnOvkNcyVV3At4Nc",
+ 
   authDomain: "marubharuch.firebaseapp.com",
-  databaseURL: "https://marubharuch.firebaseio.com",
-  projectId: "marubharuch",
-  storageBucket: "marubharuch.appspot.com",
-  messagingSenderId: "307625591423",
-  appId: "1:307625591423:web:7e783b81ed5aba4f"
+ 
 };
 firebase.initializeApp(config);
 
